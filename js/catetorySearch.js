ced0b2a94d84b078a47e2e39209add4d67f481a0
lex-xin <173311763@qq.com>
2017-01-07 09:02:05 +0800

    changefile

--- js/catetorySearch.js
+++ js/catetorySearch.js
@@ -1,4 +1,15 @@
-$(".category-type a").click(function(event) {
-	var _self = $(this);
-	_self.addClass('selected').siblings().removeClass('selected');
+// 内容部分滑动
+var catetorySwiper = new Swiper(".category-box", {
+    initialSlide: 0,
+    loop: false,
+    speed: 400,
+    onSlideChangeStart: function(){
+        $(".category-type .selected").removeClass('selected');
+        $(".category-type a").eq(catetorySwiper.activeIndex).addClass('selected');
+    }
+})
+$(".category-type a").on("touchstart mousedown", function(e){
+    var _self = $(this);
+    _self.addClass('selected').siblings().removeClass('selected');
+    catetorySwiper.slideTo(_self.index())
 });
